2d929b551e0a052aa8f23a7847e5f2cf069a95e6
wei <che-wei.lin@sjsu.edu>
2018-02-09 17:11:00 -0800

    first commit

---
+++ lib/AWS/version.rb
@@ -0,0 +1,3 @@
+module AWS
+  VERSION = "0.1.0"
+end
---
+++ spec/AWS_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe AWS do
+  it 'has a version number' do
+    expect(AWS::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
